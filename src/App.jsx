e17d009a0094437ec74077fd63923e1f0bd9bf96
Dinesh <dineshigdd@gmail.com>
2025-06-29 23:43:03 -0700

    add initial set up files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <div className='app'>
+        <nav>Navvar</nav>
+        <main>All Routing</main>
+    </div>
+  )
+}
+
+export default App
