1d9a05ca2ae63832fb91aecd15c8d34b00331d6b
Dinesh <dineshigdd@gmail.com>
2025-07-01 22:06:31 -0700

    add navbar lonks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './Navbar.css'
 import rocket from '../../assets/rocket.png'
+import star from '../../assets/glowing-star.png'
+import idButton from '../../assets/id-button.png'
+import memo from '../../assets/memo.png'
+import order from '../../assets/package.png'
+import lock from '../../assets/locked.png'
+
+import LinkWithIcon from './LinkWithIcon'
 
 const Navbar = () => {
   return (
@@ -13,8 +20,14 @@ const Navbar = () => {
             </form>      
         </div>   
         <div className='align_center navbar_links'>
-            <a href='#' className='align_center'>
-                Home <img src={ rocket } alt='' className='link_emoji' />
+            <LinkWithIcon title="Home" link="/" emoji={ rocket } />
+            <LinkWithIcon title="Products" link="/products" emoji={ star } />
+            <LinkWithIcon title="LogIn" link="/login" emoji={ idButton } />
+            <LinkWithIcon title="SignUp" link="/signup" emoji={ memo } />
+            <LinkWithIcon title="My Orders" link="/myorders" emoji={ order } />
+            <LinkWithIcon title="Logout" link="/logout" emoji={ lock } />
+            <a href='/cart' className='align_center'>
+                Cart <p className='align_center cart_counts'>0</p>            
             </a>
         </div>
     </nav> 
