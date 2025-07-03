fd4ada2326aa2767da236ddbfc47a7133fefa669
Dinesh <dineshigdd@gmail.com>
2025-07-02 22:57:47 -0700

    add frontend components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import "./App.css"
 import Navbar from './components/Navbar/Navbar'
 import HomePage from './components/Home/HomePage'
+import ProductsPage from './components/Products/ProductsPage'
+import SingleProductPage from './components/SingleProduct/SingleProductPage'
+import CartPage from './components/Cart/CartPage'
+import MyOrderPage from './components/MyOrder/MyOrderPage'
 
 const App = () => {
   return (
     <div className='app'>
         <Navbar />
         <main>
-          <HomePage />
+          {/* <HomePage /> */}
+          {/* <ProductsPage /> */}
+          {/* <SingleProductPage /> */}
+          {/* <CartPage /> */}
+          <MyOrderPage />
         </main>
     </div>
   )
