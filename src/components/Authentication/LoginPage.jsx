8c344406d0ab7f8e6a16f851c8e362f0553b7bde
Dinesh <dineshigdd@gmail.com>
2025-07-03 21:52:50 -0700

    add login form

---
+++ src/components/Authentication/LoginPage.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import './LoginPage.css'
+
+const LoginPage = () => {
+
+    const { register , handleSubmit, formState: { errors } } = useForm();
+ 
+    const onSubmit = ( formData ) => console.log( formData )
+
+    return (
+        <section className="align_center form_page">
+            <form 
+                className="authentication_form" 
+                onSubmit={ handleSubmit( onSubmit ) }>
+                <h2>Login Form</h2>
+                <div className="form_inputs">
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input 
+                            type="text" 
+                            id='name' 
+                            className='form_text_input' 
+                            placeholder='Enter your name'
+                            { ...register("name",{
+                                required: true,
+                                minLength: 3
+                            })}
+                        />
+                        { errors.name?.type === "required" && <em className='form_error'>Please enter your name</em>  }
+                        { errors.name?.type === "minLength" && <em className='form_error'>Name should be 3 or more characters</em>  }
+                    </div>
+
+                    <div>
+                        <label htmlFor="phone">Phone Number</label>
+                        <input 
+                            type='number' 
+                            id="phone" 
+                            className='form_text_input' 
+                            placeholder='Enter your phone number'
+                            { ...register("phone", { valueAsNumber : true })}
+                        />
+                        
+                    </div>                
+                </div>
+
+                <button className='search_button form_submit'>Submit</button>
+            </form>
+    </section>
+  ) 
+}
+
+export default LoginPage
