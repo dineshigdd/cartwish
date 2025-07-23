b55abe3b8c883e2e2c793ddb6d881c55490eb6d0
Dinesh <dineshigdd@gmail.com>
2025-07-23 14:17:51 -0700

    add search functionality ,auto suggestions,debouncing, and sorting

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import UserContext from '../../contexts/userContext'
 import CartContext from '../../contexts/cartContext'
@@ -11,20 +11,93 @@ import order from '../../assets/package.png'
 import lock from '../../assets/locked.png'
 
 import LinkWithIcon from './LinkWithIcon'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate, useSearchParams } from 'react-router-dom'
+import { getSuggestionsAPI } from '../../services/productServices'
 
 
 const Navbar = () => {
   const user = useContext( UserContext );
   const { cart } = useContext( CartContext );
+  const [ search, setSearch ] = useState("");
+  const [ suggestions , setSuggestions ] = useState([]);
+  const [ selectedItem , setSelectedItem ] = useState(-1);
+  const navigate = useNavigate();
+
+  const handleSubmit = ( e ) =>{
+    e.preventDefault();
+    if( search.trim() !== "" ){
+        navigate(`products?search=${ search.trim()}`)
+    }
+    setSuggestions([])
+  };
+
+  /* Debouncing: a method to delay the execution of a function until 
+  after a certain amount of time has passed. setTimeout() is used to implement debounicng*/
+  useEffect(()=>{
+    const delaySuggestions = setTimeout(() => {
+        if( search.trim()!== "" ){
+        getSuggestionsAPI( search )
+        .then( res => setSuggestions( res.data))
+        .catch( err=> console.log( err ))
+        }else{
+            setSuggestions([])
+        }
+    } , 300 )   
+
+    return () => clearTimeout( delaySuggestions )
+  }, [ search ])
+
+  const handleKeyDown = ( e ) =>{
+    if( selectedItem < suggestions.length ){
+            if( e.key === "ArrowDown"){
+                setSelectedItem( (current) => current === suggestions.length - 1  ? 0: current + 1 );
+            }
+            else if( e.key === "ArrowUp"){
+                setSelectedItem( (current) => current === 0 ? suggestions.length - 1 : current - 1 );
+            }
+            else if( e.key === "Enter" && selectedItem > -1 ){
+                const suggestion = suggestions[ selectedItem ]
+                navigate(`/products?search=${ suggestion.title }`)
+                setSearch("")
+                setSuggestions([])
+            }
+        } else {
+            setSelectedItem( -1 )
+        }
+  }
 
   return (
     <nav className='align_center navbar'>
         <div className='align_center'>
             <h1 className='navbar_heading'>CartWish</h1>
-            <form className='navbar_form'>
-                <input type='text' className='navbar_search' placeholder='Search Products'/>
+            <form className='navbar_form' onSubmit={ handleSubmit }>
+                <input 
+                    type='text' 
+                    className='navbar_search' 
+                    placeholder='Search Products' 
+                    value={ search }
+                    onChange={ e => setSearch( e.target.value )}
+                    onKeyDown={ handleKeyDown }
+                />
                 <button type='submit' className='search_button'>Search</button>
+                { suggestions.length > 0 && 
+                    <ul className="search_result">
+                        { suggestions.map( (suggestions , index ) =>(
+                            <li key={ suggestions._id }
+                                className={ selectedItem === index ? 'search_suggestion_link active': "search_suggestion_link" }>
+                                    <Link 
+                                        to={`/products?search=${ suggestions.title }`}
+                                        onClick={()=> {
+                                            setSearch(suggestions.title);
+                                            setSuggestions([])
+                                        }}
+                                        >{ suggestions.title }</Link>
+                            </li>)            
+
+                        )}
+                    </ul>
+                }
+
             </form>      
         </div>   
         <div className='align_center navbar_links'>
--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -12,19 +12,23 @@ const ProductsList = () => {
   const [ page, setPage ] = useState( 1 )
   const [ search, setSearch ] = useSearchParams();
   const category = search.get("category");
+  const searchQuery = search.get("search");
+  const [ sortBy , setSortBy ] = useState("");
+  const [ sortedProducts, setSortedProducts ] = useState([])
 
   const { data , error , isLoading } =  useData('/products' , {
       params :{
+      search: searchQuery,
       category,
       perPage:10,//tells how many products would appear in a page
       page,
     },
-  },[ category, page ]);
+  },[ searchQuery,  category, page ]);
    
 
   useEffect(()=>{    
     setPage( 1 )
-  }, [category])
+  }, [category , searchQuery ])
 
   const skeletons = [1,2,3,4,5,6,7,8];
 
@@ -53,11 +57,33 @@ const ProductsList = () => {
     return ()=> window.removeEventListener( "scroll", handleScroll )
   },[ data , isLoading ])
 
+
+  useEffect(()=>{
+    if( data && data.products ){
+      const products = [ ... data.products ]
+
+      if( sortBy === "price desc"){
+        setSortedProducts( products.sort(( a, b ) => b.price - a.price ))
+      }
+      else if( sortBy === "price asc"){
+        setSortedProducts( products.sort(( a, b ) => a.price - b.price ))
+      }
+      else if( sortBy === "rate desc"){
+         setSortedProducts( products.sort(( a, b ) => b.reviews.rate - a.reviews.rate ))
+      }
+      else if( sortBy === "rate asc"){
+         setSortedProducts( products.sort(( a, b ) => a.reviews.rate - b.reviews.rate ))
+      }else{
+        setSortedProducts( products )
+      }
+    }
+  }, [ sortBy , data ])
+
   return (
     <section className="products_list_section">
         <header className="align_center products_list_header">
             <h2>Products</h2>
-            <select name="sort" id="" className="products_sorting">
+            <select name="sort" id="" className="products_sorting" onChange={ e => setSortBy( e.target.value )}>
                 <option value="">Relevance</option>
                 <option value="price desc">Price HIGH to LOW</option>
                 <option value="price asc">Price LOW to HIGH</option>
@@ -69,7 +95,7 @@ const ProductsList = () => {
         <div className="products_list">
             { error && <em className='form_error'>{ error }</em>}
             { data?.products && 
-               ( data.products  && data.products.map( (product) => 
+               ( sortedProducts.map((product) => 
                   (
                         <ProductCard 
                           key={ product._id }
